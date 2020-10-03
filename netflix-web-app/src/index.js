d05f5e5f7ee4ceff74232e6905aac081367cf691
unknown <2609.shsumit@gmail.com>
2020-10-03 15:15:11 +0530

    basic set up

---
+++ netflix-web-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Card from './Card';
+
+ReactDom.render(
+<Card 
+sname=""
+imgscr=""
+title=""
+link=""
+/>,
+  document.getElementById('root')
+);
